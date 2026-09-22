db6b5bb2b9c58af9ac58e6abb61a85fe2844b90d
OLjuX <olaf.juszczak872@gmail.com>
2026-09-22 09:01:46 +0200

    Implement adding participants

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import type {Person} from "./types/Person"
 import PersonForm from "./components/Form/Person/PersonForm";
 
 export default function App() {
+  const [people, setPeople] = useState<Person[]>([]);
+
+  const addPerson = (name: string) => {
+    const newPerson: Person = {
+      id: crypto.randomUUID(),
+      name,
+    };
+
+    setPeople((prevPeople) => [...prevPeople, newPerson]);
+  };
+
   return (
     <>
-      <PersonForm/>
+      <PersonForm onAddPerson={addPerson}/>
     </>
   )
 }
--- src/components/Form/Person/PersonForm.tsx
+++ src/components/Form/Person/PersonForm.tsx
@@ -1,7 +1,24 @@
-function PersonForm() {
+import { useState, type FormEvent } from "react";
+
+interface PersonFormProps {
+    onAddPerson: (name: string) => void;
+}
+
+function PersonForm({onAddPerson}: PersonFormProps) {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if(!name.trim()) return;
+
+    onAddPerson(name.trim());
+    setName("");
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Enter person's name" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Enter person's name" value={name} onChange={(e) => setName(e.target.value)}/>
       <button type="submit">Add person</button>
     </form>
   );
