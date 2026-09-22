d70573c1ea8abdcca72755c826ac3796818aaf1c
OLjuX <olaf.juszczak872@gmail.com>
2026-09-22 08:46:51 +0200

    Add person form component

---
+++ src/components/Form/Person/PersonForm.tsx
@@ -0,0 +1,10 @@
+function PersonForm() {
+  return (
+    <form>
+      <input type="text" placeholder="Enter person's name" />
+      <button type="submit">Add person</button>
+    </form>
+  );
+}
+
+export default PersonForm;
